ce405ed897f582e281ac11d67aca8463a4a5a940
Maksym Dombrov <maksymdombrov@gmail.com>
2024-07-29 20:31:06 +0300

    update comments for eventHandlers.js file

--- utils/eventHandlers.js
+++ utils/eventHandlers.js
@@ -1,15 +1,17 @@
-import { animateTextTyping } from "./textAnimation.js";
-import { elementsLocation } from "./domSelects.js";
+import { animateTextTyping } from './textAnimation.js';
+import { elementsLocation } from './domSelects.js';
 
-elementsLocation.registerButton.addEventListener("click", (e) => {
+//! Navbar register button
+elementsLocation.registerButton.addEventListener('click', e => {
   e.preventDefault();
-  elementsLocation.welcomeText.classList.add("hidden");
-  elementsLocation.loginButton.classList.add("hidden");
-  elementsLocation.registerForm.classList.remove("hidden");
-  elementsLocation.registerButton.classList.add("hidden");
+  elementsLocation.welcomeText.classList.add('hidden');
+  elementsLocation.loginButton.classList.add('hidden');
+  elementsLocation.registerForm.classList.remove('hidden');
+  elementsLocation.registerButton.classList.add('hidden');
 });
 
-elementsLocation.registerSubmit.addEventListener("click", (e) => {
+//! > Submit button in register form
+elementsLocation.registerSubmit.addEventListener('click', e => {
   e.preventDefault();
 
   if (
@@ -21,11 +23,11 @@ elementsLocation.registerSubmit.addEventListener("click", (e) => {
   ) {
     return;
   }
-  elementsLocation.specialForm.classList.remove("hidden");
-  elementsLocation.registerForm.classList.add("hidden");
-  elementsLocation.specialNameGreeting.classList.remove("hidden");
-  elementsLocation.specialRemember.classList.remove("hidden");
-  elementsLocation.specialRemainingNumber.classList.remove("hidden");
+  elementsLocation.specialForm.classList.remove('hidden');
+  elementsLocation.registerForm.classList.add('hidden');
+  elementsLocation.specialNameGreeting.classList.remove('hidden');
+  elementsLocation.specialRemember.classList.remove('hidden');
+  elementsLocation.specialRemainingNumber.classList.remove('hidden');
   elementsLocation.specialNameGreeting.textContent = `Hello, ${elementsLocation.registerName.value}`;
 
   animateTextTyping(
@@ -38,29 +40,32 @@ elementsLocation.registerSubmit.addEventListener("click", (e) => {
   );
 });
 
-elementsLocation.specialSubmit.addEventListener("click", (e) => {
+//! > Submit button in special assign
+elementsLocation.specialSubmit.addEventListener('click', e => {
   e.preventDefault();
-  elementsLocation.specialForm.classList.add("hidden");
-  elementsLocation.specialNameGreeting.classList.add("hidden");
-  elementsLocation.specialRemember.classList.add("hidden");
-  elementsLocation.welcomeText.classList.remove("hidden");
-  elementsLocation.loginButton.classList.remove("hidden");
-  elementsLocation.registerButton.classList.remove("hidden");
+  elementsLocation.specialForm.classList.add('hidden');
+  elementsLocation.specialNameGreeting.classList.add('hidden');
+  elementsLocation.specialRemember.classList.add('hidden');
+  elementsLocation.welcomeText.classList.remove('hidden');
+  elementsLocation.loginButton.classList.remove('hidden');
+  elementsLocation.registerButton.classList.remove('hidden');
   animateTextTyping(
     elementsLocation.welcomeText,
     elementsLocation.welcomeText.textContent
   );
 });
 
-elementsLocation.loginButton.addEventListener("click", (e) => {
-  elementsLocation.registerButton.classList.add("hidden");
-  elementsLocation.welcomeText.classList.add("hidden");
-  elementsLocation.loginButton.classList.add("hidden");
-  elementsLocation.loginForm.classList.remove("hidden");
+//! Login button on main screen
+elementsLocation.loginButton.addEventListener('click', e => {
+  elementsLocation.registerButton.classList.add('hidden');
+  elementsLocation.welcomeText.classList.add('hidden');
+  elementsLocation.loginButton.classList.add('hidden');
+  elementsLocation.loginForm.classList.remove('hidden');
 });
 
-elementsLocation.loginSubmit.addEventListener("click", (e) => {
+//! > Submit button in login form
+elementsLocation.loginSubmit.addEventListener('click', e => {
   e.preventDefault();
-  elementsLocation.navbarElements.classList.remove("hidden");
-  elementsLocation.loginForm.classList.add("hidden");
+  elementsLocation.navbarElements.classList.remove('hidden');
+  elementsLocation.loginForm.classList.add('hidden');
 });
