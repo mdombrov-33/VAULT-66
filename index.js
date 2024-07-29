34482ac38515fc64acdea813f625325753a45b48
Maksym Dombrov <maksymdombrov@gmail.com>
2024-07-29 22:27:42 +0300

    remove parcel

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-import {
-  animateTextOnStart,
-  animateTextTyping,
-} from './utils/textAnimation.js';
+import { animateTextOnStart } from './utils/textAnimation.js';
 import { assignSpecial } from './utils/specialAssign.js';
 animateTextOnStart();
 assignSpecial();
